ec40cb660a909edd582a5d200f00c0a0359ee48c
MantoshBhawal <mantoshbhawal@gmail.com>
2021-08-24 00:43:34 +0530

    inti

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Header} from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
+class App extends React.Component {
+    render () {
+        return (
+            <GlobalProvider>
+                <Header/>
+                <div className="container">
+                    <Balance/>
+                    <IncomeExpenses/>
+                    <TransactionList/>
+                    <AddTransaction/>
+                </div>
+            </GlobalProvider>
+        )
+    }
+}
+
+export default App;
